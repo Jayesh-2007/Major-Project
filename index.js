a60b11e83d519f36b3b489b4535129b916d8452e
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2026-03-19 16:12:15 +0530

    add server side validation code

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+const listingSchema = require("./schema");
+const { validateHeaderName } = require("http");
 
 // mognoose connection
 async function main() {
@@ -31,59 +34,106 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  console.log(error);
+  if (error) {
+    let errorMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(errorMsg, 400);
+  } else {
+    next();
+  }
+};
+
 // home route
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.get("/listings", async (req, res) => {
-  const listings = await Listing.find();
-  res.render("./listings/index.ejs", { listings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const listings = await Listing.find();
+    res.render("./listings/index.ejs", { listings });
+  }),
+);
 
 // Create route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
-app.post("/listings", wrapAsync(async (req, res) => {
-
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+      throw new ExpressError(result.error, 400);
+    }
     const newListing = await new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-  }
-)); 
+  }),
+);
 
 // Show route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  }),
+);
 
 // Update route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  }),
+);
 
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError("Sent valid data for listing", 400);
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  }),
+);
 
 // Delete route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  validateHeaderName,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  }),
+);
+
+app.all("/{*splat}", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
 // error handling middleware
 app.use((err, req, res, next) => {
-  res.send("Something went wrong");
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", {
+    err,
+    statusCode,
+    message,
+  });
 });
 
 app.listen(8080, () => {
---
+++ schema.js
@@ -0,0 +1,15 @@
+const joi = require("joi");
+
+const listingSchema = joi.object({
+    listing: joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+        price: joi.number().required().min(0),
+        location: joi.string().required(),
+        image: joi.string().allow("", null),
+        country: joi.string().required(),
+        
+    }).required()
+});
+
+module.exports = listingSchema;
